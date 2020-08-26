03662e0e8b97f7dcd7989182ad8728745f0617bb
crazyones110 <crazyones110@gmail.com>
2020-08-26 17:05:07 +0800

    优化黑白名单代码

--- src/components/BAndWList.js
+++ src/components/BAndWList.js
@@ -3,26 +3,20 @@ import '../styles/BAndWList.css'
 import blackList from '../Data/blacks.json'
 import whiteList from '../Data/whites.json'
 
-function random() {
-  return Math.floor(Math.random() * 611) // 一个长度是 33884, 一个长度是 100000
+const random = () => Math.floor(Math.random() * 611) // 一个长度是 33884, 一个长度是 100000
+
+const createRandomArr = () => {
+  const array50 = new Array(50)
+  for (let i = 0; i < 50; ++i) {
+    array50[i] = random()
+  }
+  return array50
 }
 
 const BAndWList = () => {
-  const [arr50, setArr] = useState(() => {
-    const array50 = new Array(50)
-    for (let i = 0; i < 50; ++i) {
-      array50[i] = random()
-    }
-    return array50
-  })
+  const [randomArr, setArr] = useState(() => createRandomArr())
   const refresh = () => {
-    setArr(() => {
-      const array50 = new Array(50)
-      for (let i = 0; i < 50; ++i) {
-        array50[i] = random()
-      }
-      return array50
-    })
+    setArr(() => createRandomArr())
   }
   return (
     <div>
@@ -32,13 +26,13 @@ const BAndWList = () => {
       <div className="list-wrapper">
         <ul id="blacks">
           <div className="list-header">黑名单</div>
-          {arr50.map((randomNum) => {
+          {randomArr.map(randomNum => {
             return <li>{blackList[randomNum].domain}</li>
           })}
         </ul>
         <ul id="whites">
           <div className="list-header">白名单</div>
-          {arr50.map((randomNum) => (
+          {randomArr.map(randomNum => (
             <li>{whiteList[randomNum].domain}</li>
           ))}
         </ul>
